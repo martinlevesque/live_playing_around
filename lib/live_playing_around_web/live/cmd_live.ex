3840dfa56ab6ccda07da5cbb12230d6d28e86cb9
Martin Levesque <martin.levesque@ubisoft.com>
2019-03-22 15:54:13 -0400

    completion

--- lib/live_playing_around_web/live/cmd_live.ex
+++ lib/live_playing_around_web/live/cmd_live.ex
@@ -7,13 +7,32 @@ defmodule LivePlayingAroundWeb.CmdLive do
   end
 
   def mount(_session, socket) do
-    { :ok, assign(socket, cmd: "", result: "", last_cmd: nil) }
+    { :ok, assign(socket, cmd: "", result: "", last_cmd: nil, suggestions: nil) }
+  end
+
+  defp run_cmd(query) do
+    query
+      |> String.to_char_list
+      |> :os.cmd
+  end
+
+  def handle_event("suggest", %{"q" => query}, socket) when byte_size(query) <= 100 do
+    cmd_todo = "cat ~/.bash_history | grep \"^#{query}\""
+    
+    suggestions_s = run_cmd(cmd_todo)
+      |> List.to_string
+      |> String.split("\n")
+      |> Enum.take(-10)
+      |> Enum.join("\n")
+
+    {
+      :noreply,
+      update(socket, :suggestions, fn _ -> suggestions_s end)
+    }
   end
 
   def handle_event("exec_cmd", %{"q" => query}, socket) do
-    result = query
-      |> String.to_char_list
-      |> :os.cmd
+    result = run_cmd(query)
 
     {
       :noreply,
